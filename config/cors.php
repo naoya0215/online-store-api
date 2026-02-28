99590d5cd1661965973ada7ccb22ee79d3b8aee7
naoya0215 <purgatory0215@icloud.com>
2026-02-28 13:11:12 +0900

    fix cors paths

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 // config/cors.php
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000',
